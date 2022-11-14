e5e6bbcca4a0d965b079da9cb3e74a08143a05d1
syorito-hatsuki <enderdron3@gmail.com>
2022-11-14 22:02:48 +0200

    Enabled serialization

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/Project.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/Project.kt
@@ -1,7 +1,10 @@
 package dev.syoritohatsuki.fstatsbackend.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Project(
-    val id: Int = -1,
+    val id: Int?,
     val name: String,
     val ownerId: Int
 )
