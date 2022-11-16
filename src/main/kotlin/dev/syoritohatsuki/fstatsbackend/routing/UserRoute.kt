b43e1fad27d6e3c33815c4f36229ec2f2669a744
syorito-hatsuki <enderdron3@gmail.com>
2022-11-17 00:18:23 +0200

    Added user self remove base (WIP)

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/routing/UserRoute.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/routing/UserRoute.kt
@@ -1,5 +1,11 @@
 package dev.syoritohatsuki.fstatsbackend.routing
 
+import dev.syoritohatsuki.fstatsbackend.dao.impl.UserDAOImpl
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.userRoute() {
@@ -7,11 +13,18 @@ fun Route.userRoute() {
         get("/{id}") {
             //TODO Get user by ID
         }
-        post {
-            //TODO Create user using body
+        get("/{username}") {
+            //TODO Get user by Name
         }
-        delete("/{id}") {
-            //TODO Delete by ID
+        authenticate("user") {
+            delete {
+                val userId = call.principal<JWTPrincipal>()!!.payload.getClaim("id").asInt()
+                //TODO Remove also user projects, metrics data and exceptions for save memory in DB
+                UserDAOImpl.deleteById(userId).let {
+                    if (it.second == 1) call.respond(HttpStatusCode.OK, "User deleted")
+                    else call.respond(HttpStatusCode.NoContent, "User not found")
+                }
+            }
         }
     }
 }
