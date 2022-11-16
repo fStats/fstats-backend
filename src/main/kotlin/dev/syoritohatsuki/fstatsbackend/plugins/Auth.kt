69d9647aa5f768add862bf02640b2582ab02c7e5
syorito-hatsuki <enderdron3@gmail.com>
2022-11-17 00:08:24 +0200

    Updated JWT Auth

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Auth.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Auth.kt
@@ -6,22 +6,27 @@ import dev.syoritohatsuki.fstatsbackend.mics.HOST
 import dev.syoritohatsuki.fstatsbackend.mics.JWT_REALM
 import dev.syoritohatsuki.fstatsbackend.mics.JWT_SECRET
 import dev.syoritohatsuki.fstatsbackend.mics.PORT
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 
 fun Application.configureAuth() {
     install(Authentication) {
-        jwt {
+        jwt("user") {
             realm = JWT_REALM
             verifier(
                 JWT.require(Algorithm.HMAC256(JWT_SECRET))
-                    .withAudience("users")
+                    .withAudience("user")
                     .withIssuer("$HOST:$PORT")
                     .build()
             )
             validate { credential ->
-                if (credential.payload.audience.contains("users")) JWTPrincipal(credential.payload) else null
+                if (credential.payload.audience.contains("user")) JWTPrincipal(credential.payload) else null
+            }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
     }
