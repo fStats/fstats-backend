f95cdf1cd514bc7bd98f1d11f37b33cd59a383dd
syorito-hatsuki <enderdron3@gmail.com>
2022-11-18 23:56:25 +0200

    Complete metric route

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/routing/MetricsRoute.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/routing/MetricsRoute.kt
@@ -1,7 +1,11 @@
 package dev.syoritohatsuki.fstatsbackend.routing
 
 import dev.syoritohatsuki.fstatsbackend.dao.impl.MetricDAOImpl
+import dev.syoritohatsuki.fstatsbackend.dao.impl.ProjectDAOImpl
+import dev.syoritohatsuki.fstatsbackend.dto.Metric
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,15 +14,28 @@ fun Route.metricsRoute() {
         get {
             call.respond(MetricDAOImpl.getAll())
         }
-        post {
-//            TODO Add new metric data to DB
-//            call.receive<Metric>()
-        }
         get("{id}") {
-//                TODO Get project metric by id in limited time range
-//                val interval = call.request.queryParameters["interval"]!!.toInt()
-//                val timeunit = TimeUnit.valueOf(call.request.queryParameters["timeunit"].toString())
-//                val projectId = call.parameters["id"]
+            val projectId = call.parameters["id"]
+
+            if (projectId == null || !Regex("^-?\\d+\$").matches(projectId)) {
+                call.respond(HttpStatusCode.BadRequest, "Incorrect project ID")
+                return@get
+            }
+
+            call.respond(MetricDAOImpl.getLastHalfYearById(projectId.toInt()))
+        }
+        post {
+            val metric = call.receive<Metric>()
+
+            if (ProjectDAOImpl.getById(metric.projectId) == null) {
+                call.respond(HttpStatusCode.NoContent, "Project not found")
+                return@post
+            }
+
+            MetricDAOImpl.add(metric).let {
+                if (it.second == 1) call.respond(HttpStatusCode.Created, "Metric data added")
+                else call.respond(HttpStatusCode.OK, "Something went wrong")
+            }
         }
     }
 }
