b69dec7cc2fcf9c079c51b2714948e2b0d0cd13f
syorito-hatsuki <enderdron3@gmail.com>
2022-11-20 22:12:32 +0200

    Fixed crash if .env file not found

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Environment.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Environment.kt
@@ -4,7 +4,9 @@ import io.github.cdimascio.dotenv.dotenv
 import kotlin.reflect.KProperty
 
 class Environment(private val defaultValue: String = "") {
-    private val dotenv = dotenv()
+    private val dotenv = dotenv {
+        ignoreIfMissing = true
+    }
 
     operator fun getValue(th: Any?, prop: KProperty<*>): String = dotenv[prop.name]
         ?: System.getenv(prop.name)
