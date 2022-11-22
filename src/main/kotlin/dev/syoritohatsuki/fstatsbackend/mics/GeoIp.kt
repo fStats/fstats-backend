a4f47211712ac675b648e70aaedae66ebfebbf23
syorito-hatsuki <enderdron3@gmail.com>
2022-11-22 18:44:43 +0200

    Fixed serialization

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
@@ -1,6 +1,7 @@
 package dev.syoritohatsuki.fstatsbackend.mics
 
 import dev.syoritohatsuki.fstatsbackend.plugins.json
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import java.net.URL
 
@@ -8,6 +9,7 @@ fun String.getGeolocationByIp(): GeoIp? = runCatching<GeoIp> {
     json.decodeFromString(URL("http://ip-api.com/json/$this?fields=status,country").readText())
 }.getOrNull()
 
+@Serializable
 data class GeoIp(
     val status: String,
     val country: String,
