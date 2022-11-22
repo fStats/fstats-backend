71a3f060813e975e016115fc5320715dba66d7e9
syorito-hatsuki <enderdron3@gmail.com>
2022-11-22 18:10:19 +0200

    Updated GeoIp functionality #2

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
@@ -4,8 +4,8 @@ import dev.syoritohatsuki.fstatsbackend.plugins.json
 import kotlinx.serialization.decodeFromString
 import java.net.URL
 
-fun String.getGeolocationByIp(): GeoIp? = runCatching {
-    return json.decodeFromString(URL("http://ip-api.com/json/$this?fields=status,country").readText())
+fun String.getGeolocationByIp(): GeoIp? = runCatching<GeoIp> {
+    json.decodeFromString(URL("http://ip-api.com/json/$this?fields=status,country").readText())
 }.getOrNull()
 
 data class GeoIp(
