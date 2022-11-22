b98d527694aeb8ae0f2544e95faf701bbc67e1a3
syorito-hatsuki <enderdron3@gmail.com>
2022-11-22 10:00:56 +0200

    Added white list for RateLimit

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/RateLimiting.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/RateLimiting.kt
@@ -3,6 +3,7 @@ package dev.syoritohatsuki.fstatsbackend.plugins
 import dev.forst.ktor.ratelimiting.RateLimiting
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
+import io.ktor.server.request.*
 import java.time.Duration
 
 fun Application.configureRateLimiting() {
@@ -12,6 +13,10 @@ fun Application.configureRateLimiting() {
             window = Duration.ofMinutes(1)
         ) {
             request.origin.host
+        }
+        excludeRequestWhen {
+            request.path().endsWith("metrics")
+            request.path().endsWith("exceptions")
         }
     }
 }
