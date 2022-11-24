cd1bf52e751e531912c2395f9fa626b7b8bae30f
syorito-hatsuki <enderdron3@gmail.com>
2022-11-24 23:14:13 +0200

    Fixed Serialization provider

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/Exception.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/Exception.kt
@@ -1,5 +1,8 @@
 package dev.syoritohatsuki.fstatsbackend.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Exception(
     val timestampSeconds: Long? = null,
     val projectId: Int,
