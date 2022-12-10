929151b686a1cd00ffbe48d917490c5f918950d7
syorito-hatsuki <enderdron3@gmail.com>
2022-12-10 18:21:55 +0200

    Extracted module

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/Application.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/Application.kt
@@ -4,19 +4,27 @@ import dev.syoritohatsuki.fstatsbackend.mics.HOST
 import dev.syoritohatsuki.fstatsbackend.mics.PORT
 import dev.syoritohatsuki.fstatsbackend.mics.checkDatabaseConnection
 import dev.syoritohatsuki.fstatsbackend.plugins.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, host = HOST, port = PORT.toIntOrNull() ?: 1540) {
-        configureAuth()
-        configureCaching()
-        configureCors()
-        configureLogging()
-        configureRateLimiting()
-        configureRouting()
-        configureSerialization()
+    embeddedServer(
+        Netty,
+        host = HOST,
+        port = PORT.toIntOrNull() ?: 1540,
+        module = Application::fStatsModule
+    ).start(wait = true)
+}
 
-        checkDatabaseConnection()
-    }.start(wait = true)
+fun Application.fStatsModule() {
+    configureAuth()
+    configureCaching()
+    configureCors()
+    configureLogging()
+    configureRateLimiting()
+    configureRouting()
+    configureSerialization()
+
+    checkDatabaseConnection()
 }
