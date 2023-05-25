ecc829b7c134d21fdee77747d46e76855fc66cd3
syorito-hatsuki <enderdron3@gmail.com>
2023-05-26 00:38:04 +0300

    Marked GeoIp.kt as deprecated

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/GeoIp.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.jsonObject
 import java.net.URL
 
+@Deprecated("Move to library side with new API: https://ip2c.org/<IP ADDRESS>")
 fun String.getGeolocationByIp(): String = runCatching<String> {
     json.decodeFromString<JsonElement>(URL("http://ip-api.com/json/$this?fields=status,country").readText()).jsonObject["country"].toString()
 }.getOrNull() ?: "unknown"
