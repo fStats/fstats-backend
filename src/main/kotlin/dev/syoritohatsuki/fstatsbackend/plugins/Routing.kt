cc6ebbc39e3d7205cf4073a17e68c10f4645cc03
syorito-hatsuki <enderdron3@gmail.com>
2023-05-26 00:43:50 +0300

    Updated routing

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Routing.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Routing.kt
@@ -1,26 +1,22 @@
 package dev.syoritohatsuki.fstatsbackend.plugins
 
-import dev.syoritohatsuki.fstatsbackend.routing.v1.exceptionsRoute
 import dev.syoritohatsuki.fstatsbackend.routing.v1.indexRoute
 import dev.syoritohatsuki.fstatsbackend.routing.v1.metricsRoute
-import dev.syoritohatsuki.fstatsbackend.routing.v1.projectsRoute
 import dev.syoritohatsuki.fstatsbackend.routing.v2.authRoute
+import dev.syoritohatsuki.fstatsbackend.routing.v2.projectsRoute
 import dev.syoritohatsuki.fstatsbackend.routing.v2.usersRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import dev.syoritohatsuki.fstatsbackend.routing.v1.usersRoute as deprecatedUsersRoute
 
 fun Application.configureRouting() {
     routing {
         indexRoute()
         route("v1") {
-            authRoute()
-            exceptionsRoute()
             metricsRoute()
-            projectsRoute()
-            deprecatedUsersRoute()
         }
         route("v2") {
+            authRoute()
+            projectsRoute()
             usersRoute()
         }
     }
