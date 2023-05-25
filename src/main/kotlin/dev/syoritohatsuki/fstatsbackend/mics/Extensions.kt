bc952e49a6ac088a9d3e4be4ce3ab4df385c4f71
syorito-hatsuki <enderdron3@gmail.com>
2023-05-26 00:41:58 +0300

    Removed getProjectId extension

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Extensions.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Extensions.kt
@@ -1,17 +1,9 @@
 package dev.syoritohatsuki.fstatsbackend.mics
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 
 fun ApplicationCall.getRemoteIp(): String = request.headers["X-Real-IP"] ?: request.origin.remoteHost
-
-fun Parameters.getProjectId(): Int? = this["id"]?.toIntOrNull().let {
-    when (it) {
-        is Int -> it
-        else -> null
-    }
-}
 
 @Deprecated("Will be moved to Front-end")
 fun Char.getOs(): String = when (this) {
