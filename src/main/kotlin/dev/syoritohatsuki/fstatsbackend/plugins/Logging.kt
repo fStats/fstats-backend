ed1f5c66c3580b3e3bd0a2b382ec11a3202184e5
syorito-hatsuki <enderdron3@gmail.com>
2023-05-26 22:00:34 +0300

    Updated logger config

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Logging.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Logging.kt
@@ -1,7 +1,7 @@
 package dev.syoritohatsuki.fstatsbackend.plugins
 
-import dev.syoritohatsuki.fstatsbackend.mics.getRemoteIp
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
 import org.slf4j.event.Level
@@ -13,7 +13,7 @@ fun Application.configureLogging() {
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
             val userAgent = call.request.headers["User-Agent"]
-            val remoteHost = call.getRemoteIp()
+            val remoteHost = call.request.headers["X-Real-IP"] ?: call.request.origin.remoteHost
             val path = call.request.path()
             "[$remoteHost] $userAgent ($httpMethod)[$status] $path"
         }
