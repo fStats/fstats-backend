c1d15c32bf20d52eb31fadd38caca08f5e6ddbb8
syorito-hatsuki <enderdron3@gmail.com>
2023-05-30 01:07:58 +0300

    Added Hikari Connection Pool for database optimize

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
     implementation("ch.qos.logback", "logback-classic", "1.4.4")
 
     implementation("org.postgresql", "postgresql", "42.2.8")
+
+    implementation("com.zaxxer", "HikariCP", "5.0.1")
 }
