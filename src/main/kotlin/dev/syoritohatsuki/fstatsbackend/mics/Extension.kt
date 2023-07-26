f4b378ce1c183e438f4314a133c4c065552f5eb3
syorito-hatsuki <enderdron3@gmail.com>
2023-07-26 17:59:43 +0300

    Fixed serialization crash

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Extension.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Extension.kt
@@ -3,12 +3,14 @@ package dev.syoritohatsuki.fstatsbackend.mics
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import kotlinx.serialization.Serializable
 
 suspend inline fun <reified T> ApplicationCall.respondMessage(httpStatusCode: HttpStatusCode, message: T) {
-    respond(
-        httpStatusCode, mapOf(
-            "code" to httpStatusCode.value,
-            "message" to message
-        )
-    )
+    respond(httpStatusCode, Message(httpStatusCode.value, message))
 }
+
+@Serializable
+data class Message<T>(
+    val code: Int,
+    val message: T
+)
