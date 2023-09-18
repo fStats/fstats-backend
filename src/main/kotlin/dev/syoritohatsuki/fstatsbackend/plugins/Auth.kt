048df2ac85793638d39c945a3ee551b61c3fa4c7
syorito-hatsuki <enderdron3@gmail.com>
2023-09-18 23:40:20 +0300

    Changed message for 401

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Auth.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/plugins/Auth.kt
@@ -2,15 +2,11 @@ package dev.syoritohatsuki.fstatsbackend.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import dev.syoritohatsuki.fstatsbackend.mics.HOST
-import dev.syoritohatsuki.fstatsbackend.mics.JWT_REALM
-import dev.syoritohatsuki.fstatsbackend.mics.JWT_SECRET
-import dev.syoritohatsuki.fstatsbackend.mics.PORT
+import dev.syoritohatsuki.fstatsbackend.mics.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.response.*
 
 fun Application.configureAuth() {
     install(Authentication) {
@@ -25,7 +21,7 @@ fun Application.configureAuth() {
                 if (credential.payload.getClaim("username").asString() != "") JWTPrincipal(credential.payload) else null
             }
             challenge { _, _ ->
-                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+                call.respondMessage(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
     }
