9e65ee4f68bcf250a6f4babe89a76ef326b7dfe8
syorito-hatsuki <enderdron3@gmail.com>
2024-05-23 19:53:00 +0300

    SQL Inject fixing(?)

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dao/impl/FavoriteDAOImpl.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dao/impl/FavoriteDAOImpl.kt
@@ -8,21 +8,19 @@ import dev.syoritohatsuki.fstatsbackend.mics.Database.update
 object FavoriteDAOImpl : FavoriteDAO {
     override fun getUserFavorites(userId: Int): List<Project> = mutableListOf<Project>().apply {
         query("SELECT Projects.id, Projects.name, Projects.owner_id, Users.username FROM Projects JOIN favorites ON Projects.id IN(favorites.project_id) JOIN Users ON Projects.owner_id IN(Users.id) WHERE favorites.user_id IN($userId)") { resultSet ->
-            while (resultSet.next()) {
-                add(
-                    Project(
-                        resultSet.getInt("id"), resultSet.getString("name"), Project.ProjectOwner(
-                            resultSet.getInt("owner_id"), resultSet.getString("username")
-                        )
+            while (resultSet.next()) add(
+                Project(
+                    resultSet.getInt("id"), resultSet.getString("name"), Project.ProjectOwner(
+                        resultSet.getInt("owner_id"), resultSet.getString("username")
                     )
                 )
-            }
+            )
         }
     }
 
     override fun addProjectToFavorites(userId: Int, projectId: Int): Int =
-        update("INSERT INTO favorites(user_id, project_id) VALUES ($userId, $projectId)")
+        update("INSERT INTO favorites(user_id, project_id) VALUES ('$userId', '$projectId')")
 
     override fun removeProjectFromFavorites(userId: Int, projectId: Int): Int =
-        update("DELETE FROM favorites WHERE user_id = $userId AND project_id = $projectId")
+        update("DELETE FROM favorites WHERE user_id = '$userId' AND project_id = '$projectId'")
 }
