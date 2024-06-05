dbb73faadf27ec22e1f7692b49f2b8ce8abee086
syorito-hatsuki <enderdron3@gmail.com>
2024-06-05 23:21:39 +0300

    Update Metric Types

---
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/Metrics.kt
@@ -0,0 +1,24 @@
+package dev.syoritohatsuki.fstatsbackend.dto
+
+import kotlinx.serialization.Serializable
+
+typealias MetricLine = Pair<List<Long>, List<Int>>
+typealias MetricPie = Map<String, Map<String?, Int>>
+
+@Serializable
+data class Metrics(
+    val projectIds: Map<Int, String>,
+    val metric: Metric
+) {
+    @Serializable
+    data class Metric(
+        val timestampSeconds: Long? = null,
+        val projectId: Int? = null,
+        val minecraftVersion: String = "unknown",
+        val isOnlineMode: Boolean,
+        val modVersion: String = "",
+        val os: Char,
+        val location: String = "unknown",
+        val fabricApiVersion: String?
+    )
+}
