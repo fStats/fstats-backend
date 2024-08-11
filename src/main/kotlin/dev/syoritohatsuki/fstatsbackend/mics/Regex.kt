67d4a08ead7851db3676e953d77eef483dcd40e2
syorito-hatsuki <enderdron3@gmail.com>
2024-08-11 21:51:37 +0300

    Updated Regexes

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Regex.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/mics/Regex.kt
@@ -1,4 +1,12 @@
 package dev.syoritohatsuki.fstatsbackend.mics
 
-const val USERNAME_REGEX = "^([a-zA-Z0-9_]).{3,16}\$"
+import org.intellij.lang.annotations.Language
+
+@Language("RegExp")
+const val USERNAME_REGEX = "^(\\w){3,16}$"
+
+@Language("RegExp")
+const val PROJECT_REGEX = "^[\\w\\s]{1,48}\$"
+
+@Language("RegExp")
 const val PASSWORD_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).{8,64}\$"
