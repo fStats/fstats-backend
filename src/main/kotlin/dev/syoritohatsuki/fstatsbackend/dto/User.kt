af9a0961ef94c6ef80efbdd39115aeff47f543a6
syorito-hatsuki <enderdron3@gmail.com>
2024-08-18 17:35:02 +0300

    Added method User::fromResultRow

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/User.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/dto/User.kt
@@ -1,6 +1,8 @@
 package dev.syoritohatsuki.fstatsbackend.dto
 
+import dev.syoritohatsuki.fstatsbackend.db.UsersTable
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.ResultRow
 
 @Serializable
 data class User(
@@ -10,4 +12,12 @@ data class User(
     val passwordHash: String = ""
 ) {
     fun getWithoutPassword(): User = User(id, username)
+
+    companion object {
+        fun fromResultRow(resultRow: ResultRow): User = User(
+            id = resultRow[UsersTable.id].value,
+            username = resultRow[UsersTable.username],
+            passwordHash = resultRow[UsersTable.passwordHash]
+        )
+    }
 }
--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/repository/postgre/PostgresUserRepository.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/repository/postgre/PostgresUserRepository.kt
@@ -23,35 +23,23 @@ object PostgresUserRepository : UserRepository {
     }
 
     override suspend fun deleteById(id: Int): Int = dbQuery {
-        val deleteResult = UsersTable.deleteWhere { UsersTable.id eq id }
+        val deleteResult = UsersTable.deleteWhere(1) { UsersTable.id eq id }
         if (deleteResult > 0) SUCCESS else FAILED
     }
 
     override suspend fun getById(id: Int): User? = dbQuery {
-        UsersTable.selectAll().where { UsersTable.id eq id }.mapNotNull {
-            User(
-                id = it[UsersTable.id].value,
-                username = it[UsersTable.username],
-                passwordHash = it[UsersTable.passwordHash]
-            )
-        }.singleOrNull()
+        UsersTable.selectAll().where { UsersTable.id eq id }.mapNotNull(User::fromResultRow).singleOrNull()
     }
 
     override suspend fun getByName(username: String): User? = dbQuery {
-        UsersTable.selectAll().where { UsersTable.username eq username }.mapNotNull {
-            User(
-                id = it[UsersTable.id].value,
-                username = it[UsersTable.username],
-                passwordHash = it[UsersTable.passwordHash]
-            )
-        }.singleOrNull()
+        UsersTable.selectAll().where { UsersTable.username eq username }.mapNotNull(User::fromResultRow).singleOrNull()
     }
 
     override suspend fun updateUserData(userId: Int, user: User): Int {
         if (user.username.isBlank() && user.password.isBlank()) return FAILED
 
         return dbQuery {
-            UsersTable.update({ UsersTable.id eq userId }) {
+            UsersTable.update({ UsersTable.id eq userId }, 1) {
                 if (user.username.isNotBlank()) it[username] = user.username
                 if (user.password.isNotBlank()) it[passwordHash] = String(hash(user.password))
             }
