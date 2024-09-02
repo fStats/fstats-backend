70c05125db517c550d130f22de331a41b80c1144
syorito-hatsuki <enderdron3@gmail.com>
2024-09-02 23:27:01 +0300

    Added default value to ProjectsTable isVisible

--- src/main/kotlin/dev/syoritohatsuki/fstatsbackend/db/ProjectsTable.kt
+++ src/main/kotlin/dev/syoritohatsuki/fstatsbackend/db/ProjectsTable.kt
@@ -6,5 +6,5 @@ import org.jetbrains.exposed.sql.ReferenceOption
 object ProjectsTable : IntIdTable("projects") {
     val name = text("name")
     val ownerId = integer("owner_id").references(UsersTable.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
-    val isVisible = bool("visible")
+    val isVisible = bool("visible").default(true)
 }
