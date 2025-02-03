6817ba62f1cc5507f37486bf6137ef481c75c735
syorito-hatsuki <enderdron3@gmail.com>
2025-02-04 01:46:10 +0200

    Refactored gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "dev.syoritohatsuki"
-version = "2024.9.1"
+version = "2025.2.1"
 
 application {
     mainClass.set("$group.fstatsbackend.ApplicationKt")
@@ -18,65 +18,89 @@ repositories {
 }
 
 ktor {
+    val projectName = project.name
     docker {
         jreVersion.set(JavaVersion.VERSION_17)
-        localImageName.set("fstats-backend")
+        localImageName.set(projectName)
         imageTag.set(version.toString())
         portMappings.set(
             listOf(
                 DockerPortMapping(
-                    1540,
-                    1540,
-                    DockerPortMappingProtocol.TCP
+                    1540, 1540, DockerPortMappingProtocol.TCP
                 )
             )
         )
+        externalRegistry.set(
+            DockerImageRegistry.externalRegistry(
+                username = provider { System.getProperty("DOCKER_USERNAME") ?: error("DOCKER_USERNAME is undefined") },
+                password = provider { System.getProperty("DOCKER_PASSWORD") ?: error("DOCKER_PASSWORD is undefined") },
+                project = provider { projectName },
+                hostname = provider { System.getProperty("DOCKER_HOSTNAME") ?: error("DOCKER_HOSTNAME is undefined") },
+                namespace = provider { "fstats" })
+        )
     }
     fatJar {
-        archiveFileName.set("fstats-backend.jar")
+        archiveFileName.set("$projectName.jar")
     }
 }
 
 tasks {
-    delete(rootProject.layout.buildDirectory)
+    val cleanBuildDir by registering {
+        doLast {
+            delete(rootProject.layout.buildDirectory)
+        }
+    }
+
+    val loadEnv by registering {
+        doLast {
+            val envFile = file(".env")
+            if (envFile.exists()) {
+                envFile.readLines()
+                    .filter { it.isNotBlank() && !it.startsWith("#") }
+                    .forEach { line ->
+                        val (key, value) = line.split("=", limit = 2)
+                        System.setProperty(key.trim(), value.trim())
+                    }
+            } else {
+                logger.warn(".env file not found!")
+            }
+        }
+    }
+
     processResources {
+        dependsOn("loadEnv")
         filesMatching("index.html") {
-            expand(mutableMapOf("version" to project.version))
+            expand("version" to project.version)
         }
     }
 }
 
 dependencies {
     implementation("de.nycode:bcrypt:2.3.0")
-
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
 
-    implementation("io.ktor:ktor-server-auth:2.3.11")
-    implementation("io.ktor:ktor-server-auth-jwt:2.3.11")
+    // Ktor dependencies
+    val ktorVersion = "2.3.11"
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-caching-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-compression-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-rate-limit:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-swagger:$ktorVersion")
 
-    implementation("io.ktor:ktor-server-cors:2.3.11")
-    implementation("io.ktor:ktor-server-netty:2.3.11")
-
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.11")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
-
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.11")
-
-    implementation("io.ktor:ktor-server-caching-headers:2.3.11")
-    implementation("io.ktor:ktor-server-compression-jvm:2.3.11")
-    implementation("io.ktor:ktor-server-rate-limit:2.3.11")
-    implementation("io.ktor:ktor-server-status-pages:2.3.11")
-
-    implementation("io.ktor:ktor-server-swagger:2.3.11")
-
+    // Logging
     implementation("ch.qos.logback:logback-classic:1.5.7")
 
+    // Database
     implementation("org.postgresql:postgresql:42.7.2")
-
     implementation("com.zaxxer:HikariCP:5.0.1")
-
     implementation("org.jetbrains.exposed:exposed-core:0.53.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.53.0")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.53.0")
-
 }
